a136e599f95f6ba4f0b84b44d3f56c0d644e1411
SilverBoi78 <96032631+SilverBoi78@users.noreply.github.com>
2023-07-31 22:18:54 -0500

    Movement and Animation

--- RealmsOfAdventure/Assets/playerMovement.cs
+++ RealmsOfAdventure/Assets/playerMovement.cs
@@ -8,6 +8,7 @@ public class playerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
     
@@ -16,6 +17,22 @@ public class playerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //tansform local scale to x -1 when moving left
+        if (movement.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        //transform local scale to x 1 when moving right
+        else if (movement.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+       
     }
 
     void FixedUpdate()
