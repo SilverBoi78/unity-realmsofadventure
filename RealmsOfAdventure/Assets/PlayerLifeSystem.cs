cd12ff39cc676fb25809a3446ea12b59655bf995
HunterCylux <101230039+HunterCylux@users.noreply.github.com>
2023-08-13 12:18:33 -0500

    Health and GameOver
    + Hearts to show HP
    + Game Over leads to Menu after a few seconds
    - Unfortunately animation error occurs for unknown reason. Needs investigation.

--- RealmsOfAdventure/Assets/PlayerLifeSystem.cs
+++ RealmsOfAdventure/Assets/PlayerLifeSystem.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLifeSystem : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class PlayerLifeSystem : MonoBehaviour
 
     public FadingScreenAnimator fadingScreenAnimator;
 
+    public Image[] heartImages; // Array to hold heart image references
+
     private void Start()
     {
         currentHealth = maxHealth; // Initialize current health
@@ -20,9 +24,22 @@ public class PlayerLifeSystem : MonoBehaviour
     {
         currentHealth -= amount;
 
-        if (currentHealth <= 0)
+        if (currentHealth == 0)
         {
             HandlePlayerDeath();
+        }
+
+        UpdateHearts();
+    }
+
+    public void UpdateHearts()
+    {
+        for (int i = 0; i < heartImages.Length; i++)
+        {
+            if (i < currentHealth)
+                heartImages[i].enabled = true;
+            else
+                heartImages[i].enabled = false;
         }
     }
 
@@ -30,5 +47,13 @@ public class PlayerLifeSystem : MonoBehaviour
     {
         deathAnimation.SetTrigger("Death");
         fadingLife.SetFloat("FadeIn", 1f);
+        StartCoroutine(LoadMainMenu());
+    }
+
+    private IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.LoadScene("Menu");
     }
 }
